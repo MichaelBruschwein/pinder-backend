858fa4656f8fc26b187e38d9d3e1d56951b27557
Robert Crum <gargansa@hotmail.com>
2019-01-03 19:25:06 -0700

    simplified folders

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -1,71 +1,23 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import SignIn from './SignIn.js'
+import ButtonAppBar from './Top-Bar.js'
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import RegistrationForm from './RegistrationForm.js'
+
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      message: null,
-      fetching: true
-    };
-  }
-
-  componentDidMount() {
-    fetch('/api')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error(`status ${response.status}`);
-        }
-        return response.json();
-      })
-      .then(json => {
-        this.setState({
-          message: json.message,
-          fetching: false
-        });
-      }).catch(e => {
-        this.setState({
-          message: `API call failed: ${e}`,
-          fetching: false
-        });
-      })
-  }
-
   render() {
     return (
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          { process.env.NODE_ENV === 'production' ?
-              <p>
-                This is a production build from create-react-app.
-              </p>
-            : <p>
-                Edit <code>src/App.js</code> and save to reload.
-              </p>
-          }
-          <p>{'« '}<strong>
-            {this.state.fetching
-              ? 'Fetching message from API'
-              : this.state.message}
-          </strong>{' »'}</p>
-          <p><a
-            className="App-link"
-            href="https://github.com/mars/heroku-cra-node"
-          >
-            React + Node deployment on Heroku
-          </a></p>
-          <p><a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a></p>
-        </header>
+        <ButtonAppBar/>
+        {/* convention for nameing signin = login */}
+        <Route path="/Sign-In" component={SignIn} />
+        <Route path="/registration/" component={RegistrationForm}/>
       </div>
+      </Router>
     );
   }
 }
