aa9c27f15ea2bd255243edaa937127248bdcef1e
gargansa <gargansa@hotmail.com>
2019-01-10 09:04:56 -0700

    homepage logic

--- react-ui/src/Home.js
+++ react-ui/src/Home.js
@@ -1,35 +1,36 @@
 import React, { Component } from 'react';
 import './App.css';
+import { Redirect } from 'react-router-dom'
 
- 
 
 export default class Home extends Component {
 
-
-    
     constructor(props) {
         super(props);
+        //the lower two lines would be changed to represent a process of determining if a user is logged in or not
         this.pickRandom = this.pickRandom.bind(this)
         this.state = { random: 0 };
-      }
-    pickRandom(){
+    }
+    //testing function that would pick a random number to effect reroute
+    pickRandom() {
         const min = 1;
         const max = 2;
         const rand = Math.round(min + Math.random() * (max - min));
-        
+
         this.setState({ random: this.state.random + rand });
     }
-    componentDidMount(){
-        {
-            if(this.pickRandom()===1){
-                
-            }
-            
-        }
+    componentDidMount() {
+        //call the random number generator for the reroute temp function
+        this.pickRandom()
     }
 
     render() {
-        
+        //the next line would need to be changed to check and see if user is logged in
+        if (this.state.random === 1) {
+            return <Redirect to='/login' />
+        }else if(this.state.random === 2) {
+            return <Redirect to='/register' />
+        }
         return (
             <div>
                 {/* check for cookies
@@ -38,12 +39,12 @@ export default class Home extends Component {
                     if user is logged in change route to finder
                     if user is not logged in change route to login
                     */}
-                    {/* since we dont have a login yet lets randomly choose one or the other on loadin */}
-                    {/* can random work within react */}
-                    
-                    <div>The number is: {this.state.random}</div>
-                    
-          </div>
+                {/* since we dont have a login yet lets randomly choose one or the other on loadin */}
+                {/* can random work within react */}
+
+                <div>The number is: {this.state.random}</div>
+
+            </div>
         );
     }
 }
