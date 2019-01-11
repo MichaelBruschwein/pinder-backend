b830348f6477cc66c69566b555986b69b7704891
gargansa <gargansa@hotmail.com>
2019-01-10 19:38:05 -0700

    fix

--- server/app/Controllers/Http/RegisterController.js
+++ server/app/Controllers/Http/RegisterController.js
@@ -3,9 +3,7 @@
 const User = use("App/Models/User")
 
 class RegisterController {
-    // Create(){
-
-    // }
+    
     async store({ request, session, response }) {
         const user = await User.create({
             name: request.input('name'),
@@ -19,14 +17,8 @@ class RegisterController {
             age: request.input('age'),
             bio: request.input('bio')
         })
-        //const newUser = await User.create({ name, username, email, password, species, sex, city, state, age, bio })
-        // await user.save()
+        
         response.send(request.input('name'))
-        // const {name, username, email, password, species, sex, city, state, age, bio} = request.post()
-        //const newUser = await User.create({ name, username, email, password, species, sex, city, state, age, bio })
-        // let users = await User.all()
-
-        // response.send({users:user})
 
     }
 }
