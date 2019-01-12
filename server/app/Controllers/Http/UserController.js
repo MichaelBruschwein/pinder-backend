03f96090d1b13e7bae1ec4975a720151fd02110d
gargansa <gargansa@hotmail.com>
2019-01-11 17:42:10 -0700

    starting athentication

--- server/app/Controllers/Http/UserController.js
+++ server/app/Controllers/Http/UserController.js
@@ -2,6 +2,20 @@
 const User = use('App/Models/User')
 
 class UserController {
+    // this part is needed for login remembering
+    async login({ request, auth }) {
+        const { email, password } = request.all()
+        await auth.attempt(email, password)
+
+        return 'Logged in successfully'
+    }
+    show({ auth, params }) {
+        if (auth.user.id !== Number(params.id)) {
+            return 'You cannot see someone else\'s profile'
+        }
+        return auth.user
+    }
+
     async getUser({ request, response }) {
         let users = await User.all()
         response.send({
@@ -9,7 +23,7 @@ class UserController {
         })
     }
 
-    async createUser({request,response}){
+    async createUser({ request, response }) {
         response.send('create user text')
     }
 
