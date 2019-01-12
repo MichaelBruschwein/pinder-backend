4aacf66df6b73e3df214437162cfa90037feea9f
Angela Montanye <Angela.montanye@gmail.com>
2019-01-11 19:40:16 -0700

    finalizing upload form

--- react-ui/src/PhotoUploader.js
+++ react-ui/src/PhotoUploader.js
@@ -1,15 +1,35 @@
 // import React from 'react';
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import axios from "axios";
 
 export default class PhotoUploader extends Component {
+  state = { selectedFile: null };
+
+  fileChangedHandler = event => {
+    this.setState({ selectedFile: event.target.files[0] });
+  };
+
+  uploadHandler = () => {
+    console.log(this.state.selectedFile);
+    const formData = new FormData();
+    formData.append(
+      "myFile",
+      this.state.selectedFile,
+      this.state.selectedFile.name
+    );
+    axios.post("my-domain.com/file-upload", formData, {
+      onUploadProgress: progressEvent => {
+        console.log(progressEvent.loaded / progressEvent.total);
+      }
+    });
+  };
+
   render() {
     return (
       <div>
-      <input type="file" onChange={this.fileChangedHandler}>
-      </input>
-      <button onClick={this.uploadHandler}>Upload!</button>
-      </div> 
+        <input type="file" onChange={this.fileChangedHandler} />
+        <button onClick={this.uploadHandler}>Upload!</button>
+      </div>
     );
   }
 }
-
