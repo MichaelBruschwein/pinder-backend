2cdb197ba46bd582f8666a38eaf3fc2d10874d86
gargansa <gargansa@hotmail.com>
2019-01-14 13:15:05 -0700

    made profile inline

--- react-ui/src/Profile.js
+++ react-ui/src/Profile.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-import './App.css';
+import './Profile.css';
 import EditableLabel from 'react-inline-editing'
 import FormLabel from '@material-ui/core/FormLabel';
 
 export default class Profile extends Component {
     constructor(props) {
         super(props);
-        this.state=this.props.userInfo;
+        this.state = this.props.userInfo;
         this._handleFocus = this._handleFocus.bind(this);
         this._handleFocusOut = this._handleFocusOut.bind(this);
     }
@@ -21,32 +21,34 @@ export default class Profile extends Component {
 
     render() {
         return (
-            
             <div>
-                
-                <EditableLabel text={this.state.name}
+                <div className="inline">
+                    <span>Name: </span>
+                    <EditableLabel
+                        text={this.state.name}
+                        labelClassName='myLabelClass'
+                        inputClassName='myInputClass'
+                        inputWidth='200px'
+                        inputHeight='25px'
+                        inputMaxLength={50}
+                        onFocus={this._handleFocus}
+                        onFocusOut={this._handleFocusOut}
+                    />
+                </div>
+                <div className="inline">
+                    <span>Username: </span>
+                <EditableLabel className="form-control" text={this.state.username}
                     labelClassName='myLabelClass'
                     inputClassName='myInputClass'
                     inputWidth='200px'
                     inputHeight='25px'
                     inputMaxLength={50}
-                    labelFontWeight='bold'
-                    inputFontWeight='bold'
                     onFocus={this._handleFocus}
                     onFocusOut={this._handleFocusOut}
                 />
-                <FormLabel>Hello</FormLabel>
-                <EditableLabel text={this.state.username}
-                    labelClassName='myLabelClass'
-                    inputClassName='myInputClass'
-                    inputWidth='200px'
-                    inputHeight='25px'
-                    inputMaxLength={50}
-                    labelFontWeight='bold'
-                    inputFontWeight='bold'
-                    onFocus={this._handleFocus}
-                    onFocusOut={this._handleFocusOut}
-                />
+                </div>
+                <div className="inline">
+                    <span>Email: </span>
                 <EditableLabel text={this.state.email}
                     labelClassName='myLabelClass'
                     inputClassName='myInputClass'
@@ -58,6 +60,9 @@ export default class Profile extends Component {
                     onFocus={this._handleFocus}
                     onFocusOut={this._handleFocusOut}
                 />
+                </div>
+                <div className="inline">
+                    <span>Password: </span>
                 <EditableLabel text={this.state.password}
                     labelClassName='myLabelClass'
                     inputClassName='myInputClass'
@@ -69,6 +74,9 @@ export default class Profile extends Component {
                     onFocus={this._handleFocus}
                     onFocusOut={this._handleFocusOut}
                 />
+                </div>
+                <div className="inline">
+                    <span>Species: </span>
                 <EditableLabel text={this.state.species}
                     labelClassName='myLabelClass'
                     inputClassName='myInputClass'
@@ -80,6 +88,9 @@ export default class Profile extends Component {
                     onFocus={this._handleFocus}
                     onFocusOut={this._handleFocusOut}
                 />
+                </div>
+                <div className="inline">
+                    <span>Sex: </span>
                 <EditableLabel text={this.state.sex}
                     labelClassName='myLabelClass'
                     inputClassName='myInputClass'
@@ -91,18 +102,10 @@ export default class Profile extends Component {
                     onFocus={this._handleFocus}
                     onFocusOut={this._handleFocusOut}
                 />
+                </div>
+                <div className="inline">
+                    <span>City: </span>
                 <EditableLabel text={this.state.city}
-                labelClassName='myLabelClass'
-                inputClassName='myInputClass'
-                inputWidth='200px'
-                inputHeight='25px'
-                inputMaxLength={50}
-                labelFontWeight='bold'
-                inputFontWeight='bold'
-                onFocus={this._handleFocus}
-                onFocusOut={this._handleFocusOut}
-            />
-            <EditableLabel text={this.state.state}
                     labelClassName='myLabelClass'
                     inputClassName='myInputClass'
                     inputWidth='200px'
@@ -113,6 +116,23 @@ export default class Profile extends Component {
                     onFocus={this._handleFocus}
                     onFocusOut={this._handleFocusOut}
                 />
+                </div>
+                <div className="inline">
+                    <span>State: </span>
+                <EditableLabel text={this.state.state}
+                    labelClassName='myLabelClass'
+                    inputClassName='myInputClass'
+                    inputWidth='200px'
+                    inputHeight='25px'
+                    inputMaxLength={50}
+                    labelFontWeight='bold'
+                    inputFontWeight='bold'
+                    onFocus={this._handleFocus}
+                    onFocusOut={this._handleFocusOut}
+                />
+                </div>
+                <div className="inline">
+                    <span>Age: </span>
                 <EditableLabel text={this.state.age}
                     labelClassName='myLabelClass'
                     inputClassName='myInputClass'
@@ -124,6 +144,9 @@ export default class Profile extends Component {
                     onFocus={this._handleFocus}
                     onFocusOut={this._handleFocusOut}
                 />
+                </div>
+                <div className="inline">
+                    <span>Bio: </span>
                 <EditableLabel text={this.state.bio}
                     labelClassName='myLabelClass'
                     inputClassName='myInputClass'
@@ -134,7 +157,7 @@ export default class Profile extends Component {
                     inputFontWeight='bold'
                     onFocus={this._handleFocus}
                     onFocusOut={this._handleFocusOut}
-                />
+                /></div>
             </div>
         )
     }
