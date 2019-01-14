f921547cb7f1b9fc1b05516e1da22c441f8a56bc
gargansa <gargansa@hotmail.com>
2019-01-14 10:32:37 -0700

    added to profile

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -14,7 +14,18 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
-      userLoggedIn: false
+      userLoggedIn: false,
+      user:{
+      name: 'testname',
+      username: 'testusername',
+      email: 'test@test.com',
+      password: 'pass',
+      species: 'Dog',
+      sex: 'Male',
+      city: 'boz',
+      state: 'montana',
+      age: '3',
+      bio: 'Hello World'}
     }
     this.changeUserStatus = this.changeUserStatus.bind(this)
     this.userLogout = this.userLogout.bind(this)
@@ -25,21 +36,21 @@ class App extends Component {
   userLogout() {
     this.setState({ userLoggedIn: false })
   }
-render() {
-  return (
-    <Router>
-      <div className="App">
-        <Navbar userStatus={this.state.userLoggedIn} logout={this.userLogout} />
-        <Route exact path="/" render={(props) => <Home {...props} userStatus={this.state.userLoggedIn} />} />
-        <Route path="/login" render={(props) => <Login {...props} changeStatus={this.changeUserStatus} userStatus={this.state.userLoggedIn} />} />
-        <Route path="/register" render={(props) => <Register {...props} userStatus={this.state.userLoggedIn} />} />
-        <Route path="/finder" render={(props) => <Finder {...props} userStatus={this.state.userLoggedIn} />} />
-        <Route path="/matches" render={(props) => <Matches {...props} userStatus={this.state.userLoggedIn} />} />
-        <Route path="/profile" render={(props) => <Profile {...props} userStatus={this.state.userLoggedIn} />} />
-      </div>
-    </Router>
-  );
-}
+  render() {
+    return (
+      <Router>
+        <div className="App">
+          <Navbar userStatus={this.state.userLoggedIn} logout={this.userLogout} />
+          <Route exact path="/" render={(props) => <Home {...props} userStatus={this.state.userLoggedIn} />} />
+          <Route path="/login" render={(props) => <Login {...props} changeStatus={this.changeUserStatus} userStatus={this.state.userLoggedIn} />} />
+          <Route path="/register" render={(props) => <Register {...props} userStatus={this.state.userLoggedIn} />} />
+          <Route path="/finder" render={(props) => <Finder {...props} userStatus={this.state.userLoggedIn} />} />
+          <Route path="/matches" render={(props) => <Matches {...props} userStatus={this.state.userLoggedIn} />} />
+          <Route path="/profile" render={(props) => <Profile {...props} userStatus={this.state.userLoggedIn} userInfo={this.state.user} />} />
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
