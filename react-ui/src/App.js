908aeb785c6698aae8207b7f266dbd0592fb3c3a
Cory Cotterell <cordawg123@gmail.com>
2019-01-16 15:46:20 -0700

    added start to remember me feature

--- react-ui/src/App.js
+++ react-ui/src/App.js
@@ -14,92 +14,59 @@ import Profile from './Profile/Profile';
 class App extends Component {
   constructor() {
     super()
-    this.state = {
-      userLoggedIn: false,
-      user: {
-        id: '0',
-        name: 'testname',
-        username: 'testusername',
-        email: 'test@test.com',
-        password: 'pass',
-        species: 'Dog',
-        sex: 'Male',
-        city: 'boz',
-        state: 'montana',
-        age: '3',
-        bio: 'Hello World',
-        created_at: '12-12',
-        updated_at: '12-12'
+    this.rememberMe = localStorage.getItem('remember_me')
+    this.rememberMe = JSON.parse(this.rememberMe)
+    console.log(this.rememberMe)
+    if(this.rememberMe === null){
+      this.state = {
+        userLoggedIn:false,
+        user:{}
+      }
+    }else{ //the user is logged in
+      this.state = {
+        userLoggedIn:true,
+        user:this.rememberMe.user
       }
     }
     this.userLogin = this.userLogin.bind(this)
     this.userLogout = this.userLogout.bind(this)
   }
   userLogin(userData) {
-    var testObject = {token:userData.access_token.token};
-    // Put the object into storage
-    localStorage.setItem('token', JSON.stringify(testObject.token));
+    var testObject = {
+      token: userData.access_token.token,
+      user: userData.user
+    };
+    localStorage.setItem('remember_me', JSON.stringify(testObject));
 
-    // Retrieve the object from storage
-    var retrievedObject = localStorage.getItem('token');
-
-    console.log('retrievedObject: ', JSON.parse(retrievedObject));
-    
     this.setState({
       userLoggedIn: true,
       user: userData.user,
       token: userData.access_token.token
     })
   }
-  //   axios.get('/findPets', {
-  //     params: {
-  //       sex: userData.sex
-  //     }
-  //   })
-  //     .then((response) => {
-  //       this.setState({
-  //         userMatches: response.data,
-  //         userLoggedIn: true,
-  //         user: userData
-  //       })
-  //     })
-  //     .catch((error) => {
-  //       console.log(error)
-  //     })
-  // }
   userLogout() {
     localStorage.clear();
     this.setState({
-      token:"",
+      token: "",
       userLoggedIn: false,
-      user: {
-        // name: 'testname',
-        // username: 'testusername',
-        // email: 'test@test.com',
-        // password: 'pass',
-        // species: 'Dog',
-        // sex: 'Male',
-        // city: 'boz',
-        // state: 'montana',
-        // age: '3',
-        // bio: 'Hello World'
-      }
+      user: {}
     })
   }
   render() {
-    return (
-      <Router>
-        <div className="App">
-          <Navbar userStatus={this.state.userLoggedIn} logout={this.userLogout} />
-          <Route exact path="/" render={(props) => <Home {...props} userStatus={this.state.userLoggedIn} />} />
-          <Route path="/login" render={(props) => <Login {...props} changeStatus={this.userLogin} userStatus={this.state.userLoggedIn} />} />
-          <Route path="/register" render={(props) => <Register {...props} userStatus={this.state.userLoggedIn} />} />
-          <Route path="/finder" render={(props) => <Finder {...props} matches={this.state.userMatches} userStatus={this.state.userLoggedIn} />} />
-          <Route path="/profile" render={(props) => <Profile {...props} userStatus={this.state.userLoggedIn} userInfo={this.state.user} />} />
-          <Route path="/matches" render={(props) => <Matches {...props} userStatus={this.state.userLoggedIn} />} />
-        </div>
-      </Router>
-    );
+      
+      return (
+        <Router>
+          <div className="App">
+            <Navbar userStatus={this.state.userLoggedIn} logout={this.userLogout} />
+            <Route exact path="/" render={(props) => <Home {...props} userStatus={this.state.userLoggedIn} />} />
+            <Route path="/login" render={(props) => <Login {...props} changeStatus={this.userLogin} userStatus={this.state.userLoggedIn} />} />
+            <Route path="/register" render={(props) => <Register {...props} userStatus={this.state.userLoggedIn} />} />
+            <Route path="/finder" render={(props) => <Finder {...props} matches={this.state.user} userStatus={this.state.userLoggedIn} />} />
+            <Route path="/profile" render={(props) => <Profile {...props} userStatus={this.state.userLoggedIn} userInfo={this.state.user} />} />
+            <Route path="/matches" render={(props) => <Matches {...props} userStatus={this.state.userLoggedIn} />} />
+          </div>
+        </Router>
+      );
   }
 }
 
