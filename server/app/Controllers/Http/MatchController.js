515711ca34e5a67a8f37d5db450b4ffa91504d98
Cory Cotterell <cordawg123@gmail.com>
2019-01-17 12:07:16 -0700

    added logic to make matches :)

--- server/app/Controllers/Http/MatchController.js
+++ server/app/Controllers/Http/MatchController.js
@@ -12,33 +12,83 @@ class MatchController {
         // if there is a match where user1 is matched with user2 then we want to skip that create.
         let previosMatches = await Database.query().table('matches').where('user1_id', user.id)
         let matches = await Database.query().table('users').where('Sex', 'Female')
+        //we take the current users in the database compare to previous matches
+        //then we want to filter if pervious matches are equal to current users omit them
+        //the send the filtered uses into matches.
 
-        if (previosMatches.length >= 1) { // checks to see if there is any matches in database
+        if (previosMatches.length >= 1) {
 
-            previosMatches.forEach((data, i) => { // checks to see if the matches in the database already exist
-                if (data.user2_id === matches[i].id) {
-                    console.log(`user have already matches with ${data.user2_id}`)
-                }
-                // if(data.user2_id === matches.
+            function checkValue(value) {
+                // console.log(matches)
+                let check = true
+                previosMatches.forEach(element => {
+                    // console.log(element.user2_id === value.id)
+                    if (element.user2_id === value.id) {
+                        check = false
+                    }
+                });
+                return check
+            }
+            let newMatches = matches.filter(checkValue)
+
+            if (newMatches.length >= 1) {
+
+                await newMatches.forEach((user2) => {
+
+                    Match.create({
+                        user1_id: user.id,
+                        user2_id: user2.id
+                    })
+                })
+            } else {
+                response.send("no new matches")
+            }
+
+        } else {
+            await matches.forEach((user2) => {
+                Match.create({
+                    user1_id: user.id,
+                    user2_id: user2.id,
+                })
             })
         }
 
-        // let make = await matches.forEach((user2)=>{
-        //     Match.create({
-        //         user1_id:user.id,
-        //         user2_id:user2.id,
-        //     })
+
+        // previosMatches.forEach((e,i) =>{
+        //     matches.filter(checkValue)
+
         // })
-        // response.send("making them bad boys")
-
-        // We want to first make sure if there is any previous pets to not match them again.
-        // Then we want to create a row for each potiental match for the user.
-        // let match = await Match.createMany()
-        // Match.createMany
-        // then send a response back to our front end with all the matches.
-        // const {user} = request.all()
-
+        // console.log(previosMatches[0].user2_id)
+        // if (matches.length >= 1) { // checks to see if there is any matches in database
+        //     let test = matches.map((data, i) => { // checks to see if the matches in the database already exist
+        //         // console.log(previosMatches[i].user2_id)
+        //         if (data.user2_id !== previosMatches[i].user2_id) {
+        //             return data.user2_id
+        //         } else {
+        //             return null
+        //         }
+        //     })
+        //     let make = await test.forEach((user2) => {
+        //         // console.log(user2)
+        //         if (user2 === null) {
+        //             return
+        //         } else {
+        //             Match.create({
+        //                 user1_id: user.id,
+        //                 user2_id: user2.id,
+        //             })
+        //         }
+        //     })
+        // } else{
+        //      await matches.forEach((user2)=>{
+        //             Match.create({
+        //                 user1_id:user.id,
+        //                 user2_id:user2.id,
+        //             })
+        //         })
+        // }
     }
+
 }
 
 module.exports = MatchController
