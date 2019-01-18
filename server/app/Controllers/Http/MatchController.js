4e34c1282592afafa3cf2703720d553a88ae41dd
Cory Cotterell <cordawg123@gmail.com>
2019-01-17 20:51:21 -0700

    h

--- server/app/Controllers/Http/MatchController.js
+++ server/app/Controllers/Http/MatchController.js
@@ -12,12 +12,14 @@ class MatchController {
         }else{
             sex = "Male"
         }
+        
+        let previosMatches = await Database.query().table('matches').where('user1_id', user.id)
+        let matches = await Database.query().table('users').where('Sex', sex)
+
         // The user clicks some button saying find matches.
         // An axios call is called to ping to our database.
         // in this controller we want to take in the user.
         // if there is a match where user1 is matched with user2 then we want to skip that create.
-        let previosMatches = await Database.query().table('matches').where('user1_id', user.id)
-        let matches = await Database.query().table('users').where('Sex', sex)
         //we take the current users in the database compare to previous matches
         //then we want to filter if pervious matches are equal to current users omit them
         //the send the filtered uses into matches.
@@ -25,10 +27,9 @@ class MatchController {
         if (previosMatches.length >= 1) {
 
             function checkValue(value) {
-                // console.log(matches)
                 let check = true
                 previosMatches.forEach(element => {
-                    // console.log(element.user2_id === value.id)
+                   
                     if (element.user2_id === value.id) {
                         check = false
                     }
@@ -59,40 +60,6 @@ class MatchController {
             })
         }
 
-
-        // previosMatches.forEach((e,i) =>{
-        //     matches.filter(checkValue)
-
-        // })
-        // console.log(previosMatches[0].user2_id)
-        // if (matches.length >= 1) { // checks to see if there is any matches in database
-        //     let test = matches.map((data, i) => { // checks to see if the matches in the database already exist
-        //         // console.log(previosMatches[i].user2_id)
-        //         if (data.user2_id !== previosMatches[i].user2_id) {
-        //             return data.user2_id
-        //         } else {
-        //             return null
-        //         }
-        //     })
-        //     let make = await test.forEach((user2) => {
-        //         // console.log(user2)
-        //         if (user2 === null) {
-        //             return
-        //         } else {
-        //             Match.create({
-        //                 user1_id: user.id,
-        //                 user2_id: user2.id,
-        //             })
-        //         }
-        //     })
-        // } else{
-        //      await matches.forEach((user2)=>{
-        //             Match.create({
-        //                 user1_id:user.id,
-        //                 user2_id:user2.id,
-        //             })
-        //         })
-        // }
     }
 
 }
