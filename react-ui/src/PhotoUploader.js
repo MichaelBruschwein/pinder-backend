1d3fe830c366f36a18a9bf855310db8c12416e04
Angela Montanye <Angela.montanye@gmail.com>
2019-01-17 20:23:18 -0700

    S3 shtuff

--- react-ui/src/PhotoUploader.js
+++ react-ui/src/PhotoUploader.js
@@ -1,114 +1,40 @@
-// import React from 'react';
-import React, { Component } from "react";
-import axios from "axios";
-// import {Image, Video, Transformation, CloudinaryContext} from 'cloudinary-react';
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class PhotoUploader extends Component {
-  state = { selectedFile: null };
+  constructor () {
+    super();
+    this.state = {
+      file: null
+    };
+  }
 
-  fileChangedHandler = event => {
-    this.setState({ selectedFile: event.target.files[0] });
-  };
-
-  uploadHandler = () => {
-    console.log(this.state.selectedFile);
+  submitFile = (event) => {
+    event.preventDefault();
     const formData = new FormData();
-    formData.append(
-      "myFile",
-      this.state.selectedFile,
-      this.state.selectedFile.name
-    );
-    axios.post("http://res.cloudinary.com/demo/image/upload/", formData, {
-      onUploadProgress: progressEvent => {
-        console.log(progressEvent.loaded / progressEvent.total);
+    formData.append('file', this.state.file[0]);
+    axios.post(`/test-upload`, formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data'
       }
+    }).then(response => {
+      // handle your response;
+    }).catch(error => {
+      // handle your error
     });
-  };
+  }
 
-  render() {
+  handleFileUpload = (event) => {
+    this.setState({file: event.target.files});
+  }
+
+  render () {
     return (
-      <div>
-        <input type="file" onChange={this.fileChangedHandler} />
-        <button onClick={this.uploadHandler}>Upload Photo!</button>
-
-        {/* Potentially below is an upload preset func but maybe not ? */}
-        {/* <script type="text/javascript">
-    var generateSignature = function(callback, params_to_sign){
-      $.ajax({
-       url     : "http://localhost:3001/register",
-       type    : "GET",
-       dataType: "text",
-       data    : { data: params_to_sign},
-       complete: function() {console.log("complete")},
-       success : function(signature, textStatus, xhr) { callback(signature); },
-       error   : function(xhr, status, error) { console.log(xhr, status, error); }
-      })
-    }
-  </script>
-         */}
-        {/*         
-        <Image cloudName="abcm2019mcba" publicId="Pinderrrr" width="300" crop="scale" />
-        <CloudinaryContext cloudName="demo">
-  
-    <Image publicId="Pinderrroni" width="50" />
-  
-  <Image publicId="sample" width="0.5" />
-</CloudinaryContext> */}
-      </div>
+      <form onSubmit={this.submitFile}>
+        <input label='upload file' type='file' onChange={this.handleFileUpload} />
+        <button type='submit'>Send</button>
+      </form>
     );
   }
 }
 
-//Cute little widget.
-
-// function showUploadWidget() {
-//   PhotoUploader.openUploadWidget(
-//     {
-//       cloudName: "abcm2019mcba",
-//       uploadPreset: "hhblxxm0",
-//       sources: [
-//         "local",
-//         "url",
-//         "camera",
-//         "image_search",
-//         "facebook",
-//         "dropbox",
-//         "instagram"
-//       ],
-//       googleApiKey: "<image_search_google_api_key>",
-//       showAdvancedOptions: true,
-//       cropping: true,
-//       multiple: false,
-//       defaultSource: "local",
-//       styles: {
-//         palette: {
-//           window: "#000000",
-//           sourceBg: "#000000",
-//           windowBorder: "#8E9FBF",
-//           tabIcon: "#FFFFFF",
-//           inactiveTabIcon: "#8E9FBF",
-//           menuIcons: "#2AD9FF",
-//           link: "#08C0FF",
-//           action: "#336BFF",
-//           inProgress: "#00BFFF",
-//           complete: "#33ff00",
-//           error: "#EA2727",
-//           textDark: "#000000",
-//           textLight: "#FFFFFF"
-//         },
-//         fonts: {
-//           default: null,
-//           "'Acme', sans-serif": {
-//             url: "https://fonts.googleapis.com/css?family=Acme",
-//             active: true
-//           }
-//         }
-//       }
-//     },
-//     (err, info) => {
-//       if (!err) {
-//         console.log("Upload Widget event - ", info);
-//       }
-//     }
-//   );
-// }
